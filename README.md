30f018e7370effe49cf12febc3915faddc6e54f2
Erik Ripley <erikripley@gmail.com>
2018-06-26 23:40:56 -0500

    Filter the contact info to match the request params

---
+++ README.md
@@ -0,0 +1,8 @@
+# Birthdays
+- Reads the birthdays of your contacts from a VCard file
+
+# To run
+- `node app.js`
+   or
+-  `nodemon app.js`
+
--- app.js
+++ app.js
@@ -1,31 +1,60 @@
 var express = require('express');
 var app = express();
-var vCard = require('vcf');
+var vCard = require('vcardparser');
 var fs = require('fs');
-
-//var card = new vCard().parse(dd
 
 
 function parseContactsFile(vcfFile) {
-   return new Promise(function (resolve, reject) {
-      fs.readFile(vcfFile, 'utf8', function (err, data) {
-         if (err) {
-            return console.log(err);
-         }
-         resolve(data);
+   return new Promise( (resolve, reject) => {
+
+      fs.readFile(vcfFile, 'utf8', (err, allvCards) => {
+
+         var parsedContacts = [];
+         allvCards.split(/(?=BEGIN:VCARD)/).forEach( (vCardString) => {
+            vCard.parseString(vCardString, (err, json) => {
+               if (err) {
+                  return console.log(err);
+               }
+
+               ['begin', 'end', 'prodid', 'version'].forEach( (keyToRemove) => delete json[keyToRemove]);
+
+               parsedContacts.push(json);
+            });
+         });
+         resolve(parsedContacts);
       });
    });
 }
 
 
-app.get('/', function (req, res) {
-   parseContactsFile('contacts.vcf').then(function(val) {
-      res.send(val);
+
+app.get('/', (req, res) => {
+   parseContactsFile('contacts.vcf').then( parsedContacts => {
+      res.send(parsedContacts);
+   });
+});
+
+app.get('/lastname/:lastname', (req, res) => {
+   parseContactsFile('contacts.vcf').then( parsedContacts => {
+      res.send(parsedContacts.filter( contact => contact['n']['last'].toLowerCase() == req.params.lastname.toLowerCase() ));
+   });
+});
+
+app.get('/birthdays', (req, res) => {
+   parseContactsFile('contacts.vcf').then( parsedContacts => {
+      res.send(parsedContacts.filter( contact => new Date(contact['bday']).getMonth() == new Date().getMonth() ));
+   });
+});
+
+// use integer value for month
+app.get('/birthdays/:month', (req, res) => {
+   parseContactsFile('contacts.vcf').then( parsedContacts => {
+      res.send(parsedContacts.filter( contact => new Date(contact['bday']).getMonth() + 1 == req.params.month ));
    });
 });
 
 const LISTEN_PORT = 8080;
 
-app.listen(LISTEN_PORT, function () {
+app.listen(LISTEN_PORT, () => {
    console.log('listening on port', LISTEN_PORT);
 });
